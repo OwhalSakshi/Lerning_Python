74417dccaaee76f976bc0f43e64564de0e5ffead
sakshi owhal <owhalsakshi2@gmail.com>
2025-02-12 13:58:38 +0530

    fun added in if else file

--- if_else.py
+++ if_else.py
@@ -1,16 +1,16 @@
- 
-a=input("Enter your name :")
-print("Choose a day to attend 1:dj Night 2:sports 3:cltural event 4:bollywood day")
-b = int(input("enter your choise :"))
-if(b==1):
-    print("go to campus ground at 12 am ")
-elif(b==2):
-    print("go to indala sports goround ")
-elif (b==3):
-    print("go to 2 nd floor")
-elif(b==4):
-    print("go to 3rd floor ")
-else: print("invalid input")
-print(f"Thank you {a}")
+def event():
+    a=input("Enter your name :")
+    print("Choose a day to attend 1:dj Night 2:sports 3:cltural event 4:bollywood day")
+    b = int(input("enter your choise :"))
+    if(b==1):
+        print("go to campus ground at 12 am ")
+    elif(b==2):
+        print("go to indala sports goround ")
+    elif (b==3):
+        print("go to 2 nd floor")
+    elif(b==4):
+        print("go to 3rd floor ")
+    else: print("invalid input")
+    print(f"Thank you {a}")
 
-
+event()
